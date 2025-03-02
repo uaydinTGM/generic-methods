55fdd2c1289b3027862d1a365ab2118403b5ce18
Umut Aydin <uaydin@student.tgm.ac.at>
2025-03-02 15:57:59 +0100

    Kommentare hinzugefügt

--- src/GenericUtils.java
+++ src/GenericUtils.java
@@ -1,44 +1,42 @@
+/**
+ * Die Klasse GenericUtils bietet generische Hilfsmethoden für Arrays.
+ */
 public class GenericUtils {
 
+    /**
+     * Kehrt die Reihenfolge der Elemente in einem generischen Array um.
+     * @param array Das zu umkehrende Array.
+     * @param <T> Der Typ der Elemente im Array.
+     */
     public static <T> void reverse(T[] array) {
-
-
         int l = 0;
+        int r = array.length - 1;
 
-        int r = array.length -1;
-
-        while(l < r){
-
+        // Vertauscht die Elemente von außen nach innen
+        while (l < r) {
             T temp = array[l];
-
             array[l] = array[r];
-
             array[r] = temp;
 
-            l ++;
-
+            l++;
             r--;
-
-
         }
-
     }
 
-    public static <T extends Number> double sum(T[] array){
-
+    /**
+     * Berechnet die Summe der Elemente in einem Array, das Zahlen (Number) enthält.
+     * @param array Das Array mit Zahlen.
+     * @param <T> Der Typ der Zahlen im Array (muss von Number erben).
+     * @return Die Summe der Elemente als double.
+     */
+    public static <T extends Number> double sum(T[] array) {
         double sum = 0;
 
+        // Summiert die Elemente unter Verwendung von doubleValue()
         for (int i = 0; i < array.length; i++) {
-
             sum += array[i].doubleValue();
-
         }
 
         return sum;
-
-
-
-
     }
-
 }
--- src/Main.java
+++ src/Main.java
@@ -1,36 +1,37 @@
 import java.sql.Array;
 
+import java.sql.Array;
+
+/**
+ * Hauptklasse zur Demonstration der Funktionen der GenericUtils-Klasse.
+ */
 public class Main {
 
-    public static void main(String[]args){
+    public static void main(String[] args) {
 
-        String[] array = {"Max","Sebastian","Moritz","Lukas","David"};
-
+        // String-Array erstellen und ausgeben
+        String[] array = {"Max", "Sebastian", "Moritz", "Lukas", "David"};
         for (int i = 0; i < array.length; i++) {
-
             System.out.print(array[i] + " ");
-
         }
 
         System.out.println();
 
+        // String-Array umkehren und ausgeben
         GenericUtils.reverse(array);
-
         for (int i = 0; i < array.length; i++) {
-
             System.out.print(array[i] + " ");
-
         }
 
         System.out.println();
 
-        Integer[] array2 = {2,4,5,1,2};
-
+        // Integer-Array erstellen und die Summe berechnen
+        Integer[] array2 = {2, 4, 5, 1, 2};
         System.out.println(GenericUtils.sum(array2));
 
-        Byte[] array3 = {1,1,0,1,0};
-
+        // Byte-Array erstellen und die Summe berechnen
+        Byte[] array3 = {1, 1, 0, 1, 0};
         System.out.println(GenericUtils.sum(array3));
-
     }
 }
+
